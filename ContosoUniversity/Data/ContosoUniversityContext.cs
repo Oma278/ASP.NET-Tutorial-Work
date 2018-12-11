00234955924e15439ee29888a3260c6e1b9b3c85
21304416 <21304416@HSPD14083009>
2018-12-11 14:12:06 +0000

    Add project files.

---
+++ ContosoUniversity/Data/ContosoUniversityContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Models
+{
+    public class ContosoUniversityContext : DbContext
+    {
+        public ContosoUniversityContext (DbContextOptions<ContosoUniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+       // public DbSet<Student> Student { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+        public DbSet<Course> Course { get; set; }
+
+    }
+}
---
+++ ContosoUniversity/Migrations/20181211140423_AddedTelephoneToStudent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class AddedTelephoneToStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Telephone",
+                table: "Student",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telephone",
+                table: "Student");
+        }
+    }
+}
